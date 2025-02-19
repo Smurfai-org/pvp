20a64883af324f91deed48b5b48d0489b4c2a59f
RookaTu <63301074+Rokas-Mar@users.noreply.github.com>
2025-02-19 15:40:29 +0200

    added some of the components like button, tag and so on

---
+++ frontend/src/components/Checkbox.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const Checkbox = ({ children, checked=false }) => {
+  const [isChecked, setIsChecked] = useState(checked);
+
+  // Handle checkbox change event
+  const handleCheckboxChange = (e) => {
+    setIsChecked(e.target.checked);
+  };
+
+  return (
+    <div>
+      <label className="container">
+        {children}
+        <input 
+          type="checkbox" 
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <span className="checkmark"></span>
+      </label>
+    </div>
+  );
+};
+
+export default Checkbox;
