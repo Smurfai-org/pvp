1fc7d46c12394fd4de96e8d482c1b212fac5f287
Rokas <rokasmar2003@gmail.com>
2025-03-01 15:35:55 +0200

    Added dotenv, routes, and db pool

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,32 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import { fileURLToPath, pathToFileURL } from 'url';
+import path from 'path';
+import fs from 'fs';
+import 'dotenv/config';
 
 const app = express();
+const port = 5000;
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Cia yra backend");
-});
+const __filename = fileURLToPath(import.meta.url);
+const routesDirectory = path.join(path.dirname(__filename), 'routes');
 
-app.listen(5000, () => {
-    console.log('listening on port 5000');
+const files = fs.readdirSync(routesDirectory);
+
+for (const file of files) {
+    if (!file.endsWith('.js')) continue;
+  
+    const routeFilePath = pathToFileURL(path.join(routesDirectory, file)).href;
+  
+    const route = await import(routeFilePath);
+  
+    const routePath = `/${file.replace('.js', '')}`;
+  
+    app.use(routePath, route.default);
+  }
+
+app.listen(port, () => {
+    console.log('listening on port', port);
 });
