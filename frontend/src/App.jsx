6c1dee6c4128a4e634bb8847d65ce5c4c08c5f7d
Rokas <rokasmar2003@gmail.com>
2025-03-02 03:21:18 +0200

    added admin dashboard with list and create functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./pages/Register";
 import Main from "./pages/Main";
 import Navbar from "./components/NavBar";
 import AdminDash from "./pages/adminDash/adminDash";
+import CourseCreate from "./pages/CourseCreate/CourseCreate";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/admin_dash" element={<AdminDash/>}/>
+          <Route path="/create_course" element={<CourseCreate />}/>
         </Routes>
       </Router>
 
--- frontend/src/pages/AdminDash/AdminDash.jsx
+++ frontend/src/pages/AdminDash/AdminDash.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Card from '../../components/Card';
+import "./AdminDash.css";
+import Button from '../../components/Button';
+import { useNavigate } from 'react-router-dom';
+import CourseCreate from '../CourseCreate/CourseCreate';
 
 function AdminDash() {
-  return (
-    <div>adminDash</div>
-  )
+    const [courses, setCourses] = useState([]);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const req = await fetch(`http://localhost:5000/course/`, {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json' }
+                });
+                if (!req.ok) {
+                    throw new Error(`HTTP error: ${req.status}`);
+                }
+                const res = await req.json();
+
+                setCourses(res);
+            } catch (error) {
+                console.error(error.message);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleCreate = () => {
+        navigate('/create_course')
+    };
+
+    return (
+        <div className='dashboard'>
+            <Button onClick={handleCreate}>Create course</Button>
+
+            <div className="admin-dashboard">
+                {courses.map((course) => {
+                    return (
+                        <Card 
+                            key={course.id} 
+                            title={course.name} 
+                            paragraph={course.description}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
-export default AdminDash
+export default AdminDash;
