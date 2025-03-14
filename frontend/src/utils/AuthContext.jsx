8c14064cb56307023ee84f88268e22d3339d96af
RookaTu <63301074+Rokas-Mar@users.noreply.github.com>
2025-03-15 01:27:53 +0200

    added authcontext to save logged in user data

---
+++ frontend/src/utils/AuthContext.jsx
@@ -0,0 +1,72 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+const AuthContext = createContext({
+  loggedIn: false,
+  user: null,
+  loading: true,
+  logout: () => {},
+});
+
+const getData = async () => {
+  try {
+    const response = await fetch('http://localhost:5000/auth/validate', {
+      method: 'GET',
+      credentials: 'include', 
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      console.log('No valid data from backend');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+};
+
+
+export const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      const data = await getData();
+
+      if (data) {
+        setLoggedIn(true);
+        setUser(data.user);
+      } else {
+        setLoggedIn(false);
+        setUser(null);
+      }
+
+      setLoading(false);
+    };
+
+    checkLoginStatus();
+  }, []);
+
+  const logout = async () => {
+    await fetch('http://localhost:5000/auth/logout', {
+      method: 'POST',
+      credentials: 'include',
+    });
+
+    window.location.href = '/';
+  };
+
+  if (loading) return null;
+
+  return (
+    <AuthContext.Provider value={{ loggedIn, user, loading, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
