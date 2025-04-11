afd7083aba80db1908bde9cbc06e8b5b949c59d4
matbin532 <matasstaciakampis@gmail.com>
2025-04-11 18:38:02 +0300

    Fixes for hint generation and generated hint display

--- frontend/src/pages/Problem/Problem.jsx
+++ frontend/src/pages/Problem/Problem.jsx
@@ -432,7 +432,11 @@ const handleGenerateHintClick = async () => {
     const fetchHints = async () => {
       setHints([]);
       try {
-        const response = await fetch(`http://localhost:5000/hint?id=${id}`);
+        const response = await fetch(`http://localhost:5000/hint?id=${id}`, {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+        });
 
         if (!response.ok) {
           throw new Error(`Error: ${response.status}`);
