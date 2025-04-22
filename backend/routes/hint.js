530ac4bbd5b5766d7cfea2780bf3879c322b9db8
Rokas <rokasmar2003@gmail.com>
2025-04-23 00:05:16 +0300

    fixed minor errors with hints and test cases

--- backend/routes/hint.js
+++ backend/routes/hint.js
@@ -5,16 +5,20 @@ import jwt from "jsonwebtoken";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const token = req.cookies.token;
   const { id } = req.query;
-
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json();
+  }
+  const token = authHeader.split(" ")[1];
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
   try {
     let query = "SELECT * FROM hints WHERE deleted = 0";
     let params = [];
 
     if (id) {
       query += " AND fk_PROBLEMid = ? AND (fk_USERid IS NULL OR fk_USERid = ?)";
-      params.push(id, jwt.verify(token, process.env.JWT_SECRET).user.id);
+      params.push(id, decoded.user.id);
     }
 
     const [result] = await pool.execute(query, params);
@@ -32,8 +36,6 @@ router.post("/", async (req, res) => {
     return res.status(401).json();
   }
   const token = authHeader.split(" ")[1];
-  console.log("Authorization header:", authHeader);
-  console.log("Token extracted:", token);
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
   if (decoded.user.role !== "admin") {
     return res.status(403).json();
